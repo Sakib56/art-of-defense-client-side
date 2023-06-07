a9253412c4d8e1943b335bb9be8318d89e66a8f3
Sakib <sakib.hstu.bd@gmail.com>
2023-06-08 03:26:07 +0600

    slider added

--- src/Pages/Shared/Navbar/Navbar.jsx
+++ src/Pages/Shared/Navbar/Navbar.jsx
@@ -18,20 +18,20 @@ const Navbar = () => {
             .catch(error => console.error())
     }
     return (
-        <div className=''>
+        <div >
             <div className="navbar bg-gray-100 py-3 px-5">
                 <div className="navbar-start">
                     <div className="dropdown">
                         <label tabIndex={0} className="btn btn-ghost lg:hidden">
                             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                         </label>
-                        <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+                        <ul tabIndex={0}  className="menu menu-sm dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52 z-20">
                             {navOptions}
                         </ul>
                     </div>
                     <div className="btn btn-ghost normal-case text-xl">
                         <img className='w-10 h-10' src={logoImg} alt="" />
-                        <span className='text-3xl font-bold'> Art Of Defense</span>
+                        <span className='lg:text-3xl font-bold'> Art Of Defense</span>
                     </div>
                 </div>
                 <div className="navbar-center hidden lg:flex">
